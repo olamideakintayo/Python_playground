2e43cba0de178fcbdee8274bccce29fd9ce149b5
olamideakintayo <divineolamide977@gmail.com>
2025-05-13 05:17:26 +0100

    initial commit

---
+++ the_largest_of_three_values.py
@@ -0,0 +1,13 @@
+#the_largest_of_three_values.py
+
+def maximum(value1, value2, value3):
+	"""Return the maximum of three values."""
+	max_value = value2
+	if value2 > max_value:
+		max_value = value2
+	if value3 > max_value:
+		max_value = value3
+	return max_value
+	
+	
+maximum = print('Enter the values')
